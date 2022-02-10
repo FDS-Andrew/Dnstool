02154f67494c9331ae9c44797f44f20b7c03c71a
FDS-Andrew <FDS-Andrew@FDS-Andrew.01130pliuliezo01jhdof4bmvb.hx.internal.cloudapp.net>
2022-02-10 02:29:59 +0000

    fixing prep

--- prep.sh
+++ prep.sh
@@ -2,5 +2,4 @@ sudo apt-get update
 sudo apt upgrade
 sudo apt install dnsrecon
 sudo apt install whois
-echo "alias digdns='bash digdns.sh'" >> ~/.bashrc
-source ~/.bashrc
+echo "alias digdns='bash digdns.sh'" >> ~/.bash_aliases
