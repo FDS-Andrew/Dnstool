12b2d22c54a5a1be89aa76876036a192841629af
FDS-Andrew <andrewwang@freedom.net.tw>
2022-07-13 15:49:54 +0800

    add steps for install python packages in prep.sh

--- prep.sh
+++ prep.sh
@@ -2,5 +2,8 @@ sudo apt-get update
 sudo apt upgrade
 sudo apt install dnsrecon
 sudo apt install whois
+sudo apt install python3-pip
+pip install python-whois --upgrade
+pip install dnspython --upgrade
 echo "alias digdns='bash digdns.sh'" >> ~/.bash_aliases
 source ~/.bash_aliases
