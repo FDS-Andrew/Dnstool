94ac51c1a40541e125220494fd957fbdc76ea521
FDS-Andrew <andrewwang@freedom.net.tw>
2022-07-13 17:30:04 +0800

    minor fixes for mailsearch

--- mailsearchv2.py
+++ mailsearchv2.py
@@ -16,56 +16,57 @@ domain = []
 exchange = []
 f = open("maillist.txt", "r")
 for line in f:
-        test = line.split(" ")
-        domain.extend([test[0]])
-        exchange.extend([test[1]])
+    test = line.split(" ")
+    domain.extend([test[0]])
+    exchange.extend([test[1]])
 f.close()
 
 ''' mail exchange domain '''
 for rdata in m:
-        MX = [rdata.exchange]
-        pref = [rdata.preference]
-        mxlist.extend(MX)
-        preflist.extend(pref)
+    MX = [rdata.exchange]
+    pref = [rdata.preference]
+    mxlist.extend(MX)
+    preflist.extend(pref)
 mxnm = str(mxlist[(preflist.index(min(preflist)))])
 
 ''' mail server ip '''
 a = dns.resolver.resolve(mxnm, "A")
 for rdata in a:
-        mxip = [rdata]
+    mxip = [rdata]
 
 ''' compare '''
 num = 0
 f = open("maillist.txt", "r")
 for line in f:
-        if domain[num] in mxnm :
-                ANSWR = 1
-                print("\n\033[1;32;40mMail Exchange Service \n \033[0m")
-                print(exchange[num])
-                break
-        else:
-                num = num + 1
+    if domain[num] in mxnm :
+        ANSWR = 1
+        print("\n\033[1;32;40mMail Exchange Service \n \033[0m")
+        print(exchange[num])
+        break
+    else:
+        num = num + 1
 f.close()
 
 if ANSWR == 0:
-        num = 0
-        for rdata in a:
-                w = str(whois.whois(str(mxip[num])))
-                f = open("whois.txt", "w")
-                f.write(w)
-                f.close()
-                f = open("whois.txt", "r")
-                lines = f.readlines()
-                x = open("maillist.txt", "r")
-                count = 0
-                for line in x:
-                        if domain[count] in lines[1].lower():
-                                print("\n\033[1;32;40mMail Exchange Service \n \033[0m")
-                                print(exchange[count])
-                                break
-                        else:
-                                count += 1
+    num = 0
+    for rdata in a:
+        if num <= len(mxip):
+            w = str(whois.whois(str(mxip[num])))
+            f = open("whois.txt", "w")
+            f.write(w)
+            f.close()
+            f = open("whois.txt", "r")
+            lines = f.readlines()
+            x = open("maillist.txt", "r")
+            count = 0
+            for line in x:
+                if domain[count] in lines[1].lower():
+                    print("\n\033[1;32;40mMail Exchange Service \n \033[0m")
+                    print(exchange[count])
+                    ANSWR = 1
+                    break
                 else:
-                        continue
-                break
-                num += 1
+                    count += 1
+        if ANSWR == 1:
+            break
+        num += 1
