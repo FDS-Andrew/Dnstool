b68d862fbf8357ba20ac4e1dc34393b322e6c87f
FDS-Andrew <andrewwang@freedom.net.tw>
2022-07-13 16:06:40 +0800

    minor fix in prep.sh

--- prep.sh
+++ prep.sh
@@ -5,5 +5,8 @@ sudo apt install whois
 sudo apt install python3-pip
 pip install python-whois --upgrade
 pip install dnspython --upgrade
+touch ~/.bash_aliases
 echo "alias digdns='bash digdns.sh'" >> ~/.bash_aliases
+echo "alias newrecon='bash newrecon.sh'" >> ~/.bash_aliases
+echo "alias mailsearch='python3 mailsearchv2.py'" >> ~/.bash_aliases
 source ~/.bash_aliases
