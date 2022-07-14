ce4600c37628b388447d4a3b4825fae5966adf3d
FDS-Andrew <andrewwang@freedom.net.tw>
2022-07-14 11:14:11 +0800

    rewrote mailsearchv2.py to be more efficient

--- mailsearchv2.py
+++ mailsearchv2.py
@@ -1,72 +1,85 @@
-import dns.resolver
 import whois
+import dns.resolver
 
-''' enter domain name '''
-var = input("\033[1;32;40mEnter domain name \n \033[0m")
-m = dns.resolver.resolve(var, 'MX')
-mxlist = []
-preflist = []
-MX = []
-pref = []
-mxip = []
-ANSWR = 0
 
-''' load maillist '''
-domain = []
-exchange = []
-f = open("maillist.txt", "r")
-for line in f:
-    test = line.split(" ")
-    domain.extend([test[0]])
-    exchange.extend([test[1]])
-f.close()
+class MailSearch:
+    def __init__(self):
+        # import variables
+        self.mx_ip = []
+        self.domain = []
+        self.exchange = []
+        self.ans = 0
+        self.mx_name = ''
 
-''' mail exchange domain '''
-for rdata in m:
-    MX = [rdata.exchange]
-    pref = [rdata.preference]
-    mxlist.extend(MX)
-    preflist.extend(pref)
-mxnm = str(mxlist[(preflist.index(min(preflist)))])
+    def mail_list(self):
+        # format mail_list
+        read = open("mail_list.txt", "r")
+        for line in read:
+            split = line.split(" ")
+            self.domain.extend([split[0]])
+            self.exchange.extend([split[1]])
+        read.close()
 
-''' mail server ip '''
-a = dns.resolver.resolve(mxnm, "A")
-for rdata in a:
-    mxip = [rdata]
+    def mail_ip(self):
+        # find mail_server ip
+        a = dns.resolver.resolve(self.mx_name, "A")
+        for rdata in a:
+            self.mx_ip = self.mx_ip + [rdata]
 
-''' compare '''
-num = 0
-f = open("maillist.txt", "r")
-for line in f:
-    if domain[num] in mxnm :
-        ANSWR = 1
-        print("\n\033[1;32;40mMail Exchange Service \n \033[0m")
-        print(exchange[num])
-        break
-    else:
-        num = num + 1
-f.close()
+    def enter_domain(self):
+        # input domain_name
+        var = input("\033[1;32;40mEnter domain name \n \033[0m")
+        m = dns.resolver.resolve(var, 'MX')
+        mx_list = []
+        pref_list = []
+        for rdata in m:
+            mx = [rdata.exchange]
+            pref = [rdata.preference]
+            mx_list.extend(mx)
+            pref_list.extend(pref)
+        self.mx_name = str(mx_list[(pref_list.index(min(pref_list)))])
 
-if ANSWR == 0:
-    num = 0
-    for rdata in a:
-        if num <= len(mxip):
-            w = str(whois.whois(str(mxip[num])))
-            f = open("whois.txt", "w")
-            f.write(w)
-            f.close()
-            f = open("whois.txt", "r")
-            lines = f.readlines()
-            x = open("maillist.txt", "r")
-            count = 0
-            for line in x:
-                if domain[count] in lines[1].lower():
-                    print("\n\033[1;32;40mMail Exchange Service \n \033[0m")
-                    print(exchange[count])
-                    ANSWR = 1
-                    break
-                else:
-                    count += 1
-        if ANSWR == 1:
-            break
-        num += 1
+    def compare(self):
+        # compare mx_name with mail_list
+        with open("mail_list.txt", "r") as file_path:
+            for count, line in enumerate(file_path):
+                pass
+        count += 1
+        num = 0
+        while num < count:
+            if self.domain[num] in self.mx_name:
+                self.ans = 1
+                print("\n\033[1;32;40mMail Exchange Service \n \033[0m")
+                print(self.exchange[num])
+                break
+            else:
+                num += 1
+
+    def whois_mail(self):
+        # running whois on mx_ip
+        num = 0
+        while num < len(self.mx_ip):
+            w = str(whois.whois(str(self.mx_ip[num])))
+            with open("whois.txt", "w") as blank:
+                blank.write(w)
+            with open("whois.txt", "r") as read:
+                lines = read.readlines()
+                self.mx_name = lines[1].lower()
+            self.compare()
+            if self.ans == 1:
+                break
+            else:
+                num += 1
+
+    def check_ans(self):
+        # check if there's an answer, if not run whois_mail
+        if self.ans != 1:
+            self.whois_mail()
+
+
+run = MailSearch()
+run.mail_list()
+run.enter_domain()
+run.mail_ip()
+run.compare()
+run.check_ans()
