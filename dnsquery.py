61edd8e83c94a5defdc6b3673e281b4c6d036f3c
FDS-Andrew <andrewwang@freedom.net.tw>
2022-07-15 11:21:24 +0800

    add whois name_server records comparison

--- dnsquery.py
+++ dnsquery.py
@@ -1,11 +1,12 @@
 import whois
 import dns.resolver
+import re
 
 
 class Search:
     def __init__(self):
         # import variables
-        self.query_list = ["A", "AAAA", "NS", "MX", "TXT", ]
+        self.query_list = ["A", "AAAA", "NS", "MX", "TXT", "SOA"]
         self.mx_ip = []
         self.domain = []
         self.exchange = []
@@ -28,7 +29,7 @@ class Search:
             a = dns.resolver.resolve(self.mx_name, "A")
             for rdata in a:
                 self.mx_ip = self.mx_ip + [rdata]
-        except:
+        except Exception:
             pass
 
     def enter_domain(self):
@@ -46,9 +47,9 @@ class Search:
                 mx_list.extend(mx)
                 pref_list.extend(pref)
             self.mx_name = str(mx_list[(pref_list.index(min(pref_list)))])
-        except:
+        except Exception:
             self.ans = 1
-            print("\n\033[1;31;40mNo Mail Service\033[0m")
+            print("\n\033[1;31;40mNo Email Service\033[0m")
 
     def compare(self):
         # compare mx_name with mail_list
@@ -60,7 +61,7 @@ class Search:
         while num < count:
             if self.domain[num] in self.mx_name:
                 self.ans = 1
-                print("\n\033[1;32;40mMail Exchange Service\033[0m")
+                print("\n\033[1;32;40mEmail Exchange Service\033[0m")
                 print(self.exchange[num])
                 break
             else:
@@ -82,7 +83,7 @@ class Search:
                     break
                 else:
                     num += 1
-        except:
+        except Exception:
             pass
 
     def check_ans(self):
@@ -90,9 +91,11 @@ class Search:
         if self.ans != 1:
             self.whois_mail()
             if self.ans != 1:
-                print("\n\033[1;31;40mNo Email Service in Database \n \033[0m")
+                print("\n\033[1;32;40mEmail Exchange Service\033[0m")
+                print("\033[1;31;40mNo Email Service in Database \n \033[0m")
 
     def record_search(self):
+        # search for A, AAAA, NS, MX, TXT, SOA
         num = 0
         while num < len(self.query_list):
             print("\n\033[1;32;40m"+self.query_list[num]+" Records \033[0m")
@@ -100,16 +103,41 @@ class Search:
                 record = dns.resolver.resolve(self.var, self.query_list[num])
                 for rdata in record:
                     print(rdata)
-            except:
+            except Exception:
                 print("\033[1;31;40mNo "+self.query_list[num]+" Records \033[0m")
             num += 1
 
+    def ns_compare(self):
+        ans = 0
+        try:
+            w = str(whois.whois(self.var)).lower()
+            print("\n\033[1;32;40mComparing Whois name_server records \033[0m")
+            record = dns.resolver.resolve(self.var, "NS")
+            for rdata in record:
+                pattern = str(rdata)
+                if re.search(pattern, w):
+                    pass
+                else:
+                    ans = 1
+                    print("\033[1;31;40mWhois name_server records misconfiguration \033[0m")
+                    break
+            if ans == 0:
+                print("Whois name_server records correct")
+        except Exception:
+            print("\033[1;31;40mNo Whois record for comparison \033[0m")
 
-run = Search()
-run.mail_list()
-run.enter_domain()
-run.record_search()
-run.mx_name_search()
-run.mail_ip()
-run.compare()
-run.check_ans()
+
+class Steps:
+    def __init__(self):
+        run = Search()
+        run.mail_list()
+        run.enter_domain()
+        run.record_search()
+        run.ns_compare()
+        run.mx_name_search()
+        run.mail_ip()
+        run.compare()
+        run.check_ans()
+
+
+call_function = Steps()
